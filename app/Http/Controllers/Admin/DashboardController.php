7a88b0b0a94e1d277c6f8e9b17b1a612166195c3
MarcondesJuniorDev <mncjdev@gmail.com>
2024-04-23 07:49:05 -0400

    Inclusao de quantidade de usuarios no sistema

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
+    public readonly User $user;
+    public function __construct()
+    {
+        $this->user = new User();
+    }
+
     public function index(): View
     {
-        return view('admin.dashboard');
+        $countUsers = $this->user->count();
+        return view('admin.dashboard', compact('countUsers'));
     }
 }
